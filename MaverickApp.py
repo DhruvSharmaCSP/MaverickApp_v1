682ced887ca6551b5ae8b910e7f2bb8aac82a358
DhruvSharmaCSP <dhruvsharmacsp@gmail.com>
2024-12-18 15:40:23 +0100

    Update MaverickApp.py

--- MaverickApp.py
+++ MaverickApp.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def headers():
     headers = dict(request.headers)
-    return headers, 200
+    # Build a simple HTML page to display headers
+    html = "<html><body><h1>HTTP Headers</h1><table border='1'>"
+    html += "<tr><th>Header</th><th>Value</th></tr>"
+
+    for key, value in headers.items():
+        html += f"<tr><td>{key}</td><td>{value}</td></tr>"
+
+    html += "</table></body></html>"
+    return html, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
